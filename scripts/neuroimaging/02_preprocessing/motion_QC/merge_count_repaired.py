b7d3f487c92a43bd251ab875b53d2ba5d19e9452
Isaac Chen <hsuan-wei.chen@vanderbilt.edu>
2025-11-12 09:17:49 -0600

    Add script to merge count repaired CSV files and its merged output

---
+++ scripts/neuroimaging/02_preprocessing/motion_QC/merge_count_repaired.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+import pandas as pd
+
+# Set input parameters
+proj_dir = Path(__file__).resolve().parents[4]
+data_dir = proj_dir / "results" / "fMRI_count_repaired"
+
+count_repaired_csv = [
+    data_dir / "ds001486_count_repaired.csv", 
+    data_dir / "ds001894_count_repaired.csv", 
+    data_dir / "ds002236_count_repaired.csv",
+    data_dir / "ds006239_count_repaired.csv"
+]
+
+output_csv = data_dir / "merged_count_repaired.csv"
+
+# Read and concatenate
+df = pd.concat([pd.read_csv(f) for f in count_repaired_csv], ignore_index = True)
+
+# Save merged result
+df.to_csv(output_csv, index = False)
+
+print("Count repaired files merged successfully.")
